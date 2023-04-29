782a43aa7d8d476ad9498fd8ed60e1fd2ecd9483
vm2124 <vm21242124@gmail.com>
2023-04-29 14:24:26 +0530

    redux setup

---
+++ src/ReduxSetup/Reducers/UserReducer.js
@@ -0,0 +1,12 @@
+import { createReducer } from "@reduxjs/toolkit";
+const intialstate={
+    user:null
+}
+export const userReducer=createReducer(intialstate,{
+    setuser:(state,action)=>{
+        state.user=action.payload
+    },
+    deleteuser:(state)=>{
+        state.user=null;
+    }
+})
