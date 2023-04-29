272bda8b137ca8f43cc50d9a8cf4fd50060ed30b
vm2124 <vm21242124@gmail.com>
2023-04-29 22:36:24 +0530

    dashboard ui

--- src/Pages/Admin/Admincomponents/Dashboard/Dashboard.jsx
+++ src/Pages/Admin/Admincomponents/Dashboard/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import './Dashboard.css'
 const Dashboard = () => {
+  
   return (
-    <div>this is dashboard</div>
+    <div className="dashboard">
+      <div className="dashboardtop">
+        <div className="dashboardtoporderstats" style={{ "backgroundColor": "rgb(151, 141, 246)" }}>ordered</div>
+        <div style={{ "backgroundColor": "rgb(219, 255, 187)" }} className="dashboardtoporderstats">shipped</div>
+
+        <div style={{ "backgroundColor": "rgb(250, 142, 247)" }} className="dashboardtoporderstats">Delivered</div>
+        <div style={{ "backgroundColor": "rgb(255, 128, 128)" }} className="dashboardtoporderstats">Cancelled</div>
+      </div>
+      <div className="DashboardOrders">
+        <div className="DashboardOrdersTop">
+          <span className="orderHeading">sn</span><span className="orderHeading">order id</span><span className="orderHeading">custumer name</span><span className="orderHeading">status</span><span className="orderHeading">date</span><span className="orderHeading">price</span>action<span className="orderHeading"></span>
+        </div>
+        <div className="DashboardOrdersScroll">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
