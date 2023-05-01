4e10f13adb54f63c43bb19f379dec7595bad59d0
vm2124 <vm21242124@gmail.com>
2023-05-01 20:50:57 +0530

    collections

--- src/Pages/Admin/Admincomponents/categories/Categories.jsx
+++ src/Pages/Admin/Admincomponents/categories/Categories.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import './Categories.css'
 const Categories = () => {
+  const [name,setName]=useState("")
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+     axios.post('/collections/create',{name}).then((res)=>{
+      if(res.status===200){
+        alert("collection created successfully")
+      }
+     }).catch((e)=>console.log(e))
+    
+  }
+  const [collection,setCollection]=useState([]);
+  useEffect(()=>{
+    axios.get('/collections/all').then((res)=>setCollection(res.data)).catch(e=>console.log(e))
+  })
   return (
-    <div>Categories cards</div>
+    <div className="createproductForm">
+      <h1>create collection</h1>
+      <form method='POST' onSubmit={handleSubmit}>
+        <div className="createproduct">
+
+          <input
+            className="in1"
+            placeholder="collection name"
+            onChange={(e)=>setName(e.target.value)}
+            required={true}
+          />
+         
+        </div>
+        <div className="bn">
+
+          <button type="submit" className="createproductbtn">
+            Create Collection
+          </button>
+        </div>
+      </form>
+        <h1>All Categories</h1>
+      <div className="allavailablecartegories">
+        {collection.length!==0 ? collection.map((item,i)=>(
+          <div className='collections' key={i}>
+            <span>{item.name}</span>
+          </div>
+        )):
+        <div><span>not Categories yet create now</span></div>
+      }
+      </div>
+    </div>
   )
 }
 
