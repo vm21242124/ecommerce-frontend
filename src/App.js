7edc0e137b3f140a26eea63def87cb650bd70121
vm2124 <vm21242124@gmail.com>
2023-05-02 23:07:24 +0530

    routes fixing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage";
 import LoginPage from "./Pages/Login/LoginPage";
 import RegisterPage from "./Pages/Register/RegisterPage";
 import AdminDashboard from "./Pages/Admin/AdminDashboard";
 import axios from 'axios'
+import { useSelector } from "react-redux";
 
 axios.defaults.baseURL = "http://localhost:5000/api"
 axios.defaults.withCredentials = true
+
 function App() {
+  const user=useSelector((state)=>state.user)
+
+  console.log(user);
   return (
     <>
       <BrowserRouter>
@@ -15,7 +20,7 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/register" element={<RegisterPage />} />
-          <Route exact path="/admin/dashboard" element={<AdminDashboard/>}/>
+         <Route exact path="/admin/dashboard" element={<AdminDashboard/>}/>
         </Routes>
       </BrowserRouter>
     </>
