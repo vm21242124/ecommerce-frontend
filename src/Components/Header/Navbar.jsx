43eedb0ac634336e2210bf8dc7fce2015948b73f
vm2124 <vm21242124@gmail.com>
2023-05-02 23:53:25 +0530

    Navbar done

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import './Navbar.css'
-import {CiSearch} from 'react-icons/ci'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
+import { CiSearch } from 'react-icons/ci'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import {MdKeyboardArrowDown} from 'react-icons/md'
+import { useSelector } from 'react-redux'
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div className="logoside">
-            <h3>Shopee</h3>
-        </div>
-        <div className="serchbar">
-            <form className="search">
-                <input style={{"borderRadius":"10px","height":"22px"}}  placeholder='search'  type="text"className="in1" />
-                <CiSearch className='ser'/>
-            </form>
-        </div>
-        <div className="cartandlogin">
-            <div className="first">
-                <AiOutlineShoppingCart className='car'/>
-                <span>cart</span>
+    const username=useSelector((state)=>state.user.user.name)
+   
+    return (
+        <div className="navbar">
+            <div className="logoside">
+                <h3>Shopee</h3>
+            </div>
+            <div className="serchbar">
+                <form className="search">
+                    <input style={{ "borderRadius": "10px", "height": "22px" }} placeholder='search' type="text" className="in1" />
+                    <CiSearch className='ser' />
+                </form>
+            </div>
+            <div className="cartandlogin">
+
+                <AiOutlineShoppingCart className='car' />
+                <span >cart</span>
+                <span>{username? "Hi,"+ username +" ! "  :"login"}</span>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
