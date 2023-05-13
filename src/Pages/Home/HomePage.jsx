d4e388271713c13fa4e652dd15f8f4eca6a94091
vm2124 <vm21242124@gmail.com>
2023-05-13 17:55:39 +0530

    category header adding to home page

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../../Components/Header/Navbar'
 import OfferComponant from '../../Components/Offers/OfferComponant'
 import TopProducts from '../../Components/TopProduct/TopProducts'
+import CategoryHeader from '../../Components/CategoryHeader/CategoryHeader'
 
 function HomePage() {
   return (
    <div className="homepage">
     <Navbar/>
+    <CategoryHeader/>
     <OfferComponant/>
     <TopProducts/>
    </div>
