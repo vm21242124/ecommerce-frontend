c1801f33997881a9f2cc373629542e56ac5512fa
vm2124 <vm21242124@gmail.com>
2023-05-14 09:43:26 +0530

    adding to homepage

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../Components/Header/Navbar'
 import OfferComponant from '../../Components/Offers/OfferComponant'
 import TopProducts from '../../Components/TopProduct/TopProducts'
 import CategoryHeader from '../../Components/CategoryHeader/CategoryHeader'
+import MoreProducts from '../../Components/MoreProducts/MoreProducts'
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
     <CategoryHeader/>
     <OfferComponant/>
     <TopProducts/>
+    <MoreProducts/>
    </div>
   )
 }
