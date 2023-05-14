7487b36e817ef2efa18f75bcfed8a09895bfeaff
vm2124 <vm21242124@gmail.com>
2023-05-14 09:43:44 +0530

    normal styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/Home/HomePage";
 import LoginPage from "./Pages/Login/LoginPage";
 import RegisterPage from "./Pages/Register/RegisterPage";
 import AdminDashboard from "./Pages/Admin/AdminDashboard";
-import axios from 'axios'
+import axios from "axios";
 import { useSelector } from "react-redux";
+import UserCart from "./Pages/UserCart/UserCart";
 
-axios.defaults.baseURL = "http://localhost:5000/api"
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.withCredentials = true;
 
 function App() {
-  const user=useSelector((state)=>state.user)
-
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
   return (
     <>
       <BrowserRouter>
@@ -19,7 +20,10 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/register" element={<RegisterPage />} />
-         <Route exact path="/admin/dashboard" element={<AdminDashboard/>}/>
+
+          <Route exact path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route exact path="/cart" element={<UserCart/>} />
+
         </Routes>
       </BrowserRouter>
     </>
