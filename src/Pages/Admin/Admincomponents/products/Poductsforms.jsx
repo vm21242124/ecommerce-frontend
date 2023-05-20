26696c0efecda72a0288b8b6302a2f58ea39127d
vm2124 <vm21242124@gmail.com>
2023-05-20 13:16:45 +0530

    fixing some component

--- src/Pages/Admin/Admincomponents/products/Poductsforms.jsx
+++ src/Pages/Admin/Admincomponents/products/Poductsforms.jsx
@@ -4,8 +4,14 @@ import { BiEditAlt } from 'react-icons/bi'
 import { AiOutlineDelete, AiOutlineEye } from 'react-icons/ai'
 
 import axios from 'axios';
+import { appendStringAfterLength } from '../../../../UtilityFunctions';
 export const CreateProductforms = () => {
-    const [product, setProduct] = useState({ name: "", price: "", description: "", stock: 0, collectionId: "" })
+    const [product, setProduct] = useState({ name: "", price: "", description: "", stock: 0 })
+    const [collectionId, setCollectionId] = useState("")
+    const [collection, setCollection] = useState([]);
+    useEffect(() => {
+        axios.get('/collections/all').then((res) => setCollection(res.data)).catch(e => console.log(e))
+    })
     const handleChange = (e) => {
         const { name, value } = e.target;
         setProduct((prevState) => ({
@@ -21,8 +27,7 @@ export const CreateProductforms = () => {
     formdata.append('name', product.name)
     formdata.append('price', product.price)
     formdata.append('description', product.description)
-    formdata.append('collectionId', product.collectionId ? product.collectionId : "645b1fb8b5f32f21261445aa")
-
+    formdata.append('collectionId', collectionId ? collectionId : "644e505521c34bb5146019b6")
     formdata.append('stock', product.stock)
     formdata.append('file', file);
     const handleSubmit = (e) => {
@@ -75,15 +80,19 @@ export const CreateProductforms = () => {
                         onChange={handleChange}
                         required={true}
                     />
-                    <input
-                        className="in1"
-                        name='collectionId'
-
-                        placeholder="collectionId"
-                        value={product.collectionId}
-                        onChange={handleChange}
-
-                    />
+                   
+                    <h3>choose category</h3>
+                    <div className="allavailablecartegories">
+                        {collection.length !== 0 ? collection.map((item, i) => (
+                            <div onClick={() => {
+                                
+                                setCollectionId(item._id)
+                            }} className='collections' key={i}>
+                                <span>{item.name}</span>
+                            </div>
+                        )) :
+                            <div><span>No Categories</span></div>}
+                    </div>
                     <input
                         className="in1"
                         type="file"
@@ -118,7 +127,7 @@ export const GetAllProducts = () => {
     return (
         <div className="GetAllProducts">
             <div className="topindenxing">
-                <div className='topindxing1'>product id</div>
+
                 <div className='topindxing2'>Name</div>
                 <div className='topindxing3'>Price</div>
                 <div className='topindxing4'>Stock</div><div className='topindxing5'>Actions</div>
@@ -126,32 +135,50 @@ export const GetAllProducts = () => {
             </div>
             <div className="productsfromapi">
 
-                { data?.map((item, i) => (
+                {data?.map((item, i) => (
                     <Product1 key={i} name={item.name} price={item.price} stock={item.stock} id={item._id} />
-                )) }
+                ))}
             </div>
         </div>
     )
 }
 export const Product1 = (props) => {
+    const [showm, setShowm] = useState(false)
+    const handleDelete = (e) => {
+        e.preventDefault();
+        setShowm(true)
+    }
+    const confirmDelete = (e) => {
+        e.preventDefault();
 
+        axios.delete(`product/delete/${props.id}`).then(() => console.log("product deleted")).catch((e) => console.log(e.message))
+        setShowm(false)
+    }
+    const name = appendStringAfterLength(props.name, 20, "...");
     return (
-        <div className="topindenxing">
-            <div className='topindxing1'><p>{props.id}</p></div>
-            <div className='topindxing2'> <p>{props.name}</p></div>
-            <div className='topindxing3'><p>{props.price} </p></div>
-            <div className='topindxing4'><p>{props.stock}</p></div><div className='topindxing5'>
-                <span>
-                    <AiOutlineEye />
-                </span>
-                <span>
-                    <BiEditAlt />
-                </span>
-                <span >
-                    <AiOutlineDelete />
-                </span>
+        <>
+            <div className="topindenxing">
+
+                <div className='topindxing2'> <p>{name}</p></div>
+                <div className='topindxing3'><p>{props.price} </p></div>
+                <div className='topindxing4'><p>{props.stock}</p></div><div className='topindxing5'>
+                    <span>
+                        <AiOutlineEye />
+                    </span>
+                    <span>
+                        <BiEditAlt />
+                    </span>
+                    <span onClick={handleDelete}>
+                        <AiOutlineDelete />
+                    </span>
+                </div>
+
             </div>
-            
-        </div>
+            {showm ? <div className="topindenxing">
+                are you sure to delete this message
+                <button onClick={confirmDelete}>YES</button>
+                <button onClick={() => setShowm(false)}>NO</button>
+            </div> : ""}
+        </>
     )
 }
