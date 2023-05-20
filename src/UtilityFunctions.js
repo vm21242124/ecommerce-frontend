6045308408efba78e465ef0e2c17244dc07dc7ba
vm2124 <vm21242124@gmail.com>
2023-05-20 13:14:11 +0530

    utility functions

---
+++ src/UtilityFunctions.js
@@ -0,0 +1,6 @@
+export const appendStringAfterLength = (word, length, appendString) => {
+    if (word.length > length) {
+        return word.slice(0, length) + appendString;
+    }
+    return word;
+};
