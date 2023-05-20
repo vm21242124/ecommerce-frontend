371fefb45d0667ac9b15282a9afa1fe96139e061
vm2124 <vm21242124@gmail.com>
2023-05-20 13:14:54 +0530

    working on search functionality

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { CiSearch } from 'react-icons/ci'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
-import {MdKeyboardArrowDown} from 'react-icons/md'
+import { MdKeyboardArrowDown } from 'react-icons/md'
 import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 const Navbar = () => {
-    const user=useSelector((state)=>state.user.user)
-
+    const user = useSelector((state) => state.user.user)
+    
+    const [search,setSearch]=useState("")
+    const nav = useNavigate()
     return (
         <div className="navbar">
             <div className="logoside">
-                <h3>Shopee</h3>
+                <h3 style={{ cursor: "pointer" }} onClick={() => nav("/")}>Shopee</h3>
             </div>
             <div className="serchbar">
-                <form className="search">
-                    <input style={{ "borderRadius": "10px", "height": "22px" }} placeholder='search' type="text" className="in1" />
-                    <CiSearch className='ser' />
+                <form onSubmit={()=>nav(`/search/${search}`)} className="search">
+                    <input style={{ "borderRadius": "10px", "height": "22px" }} placeholder='search' type="text" className="in1" onChange={(e)=>setSearch(e.target.value)} />
+                    <CiSearch type='submit' className='ser' />
                 </form>
             </div>
             <div className="cartandlogin">
 
                 <AiOutlineShoppingCart className='car' />
-                <NavLink className="NavLink" to="/cart"><span >cart</span></NavLink> 
-                <span>{user?.name ?  <span>Hi {user.name} !</span>  : <NavLink className="NavLink" to="/login"><span>login</span></NavLink>}</span>
-                <span><MdKeyboardArrowDown/></span>
+                <NavLink className="NavLink" to="/cart"><span >cart</span></NavLink>
+                <span>{user?.name ? <span>Hi {user.name} !</span> : <NavLink className="NavLink" to="/login"><span>login</span></NavLink>}</span>
+                <span><MdKeyboardArrowDown /></span>
 
             </div>
         </div>
