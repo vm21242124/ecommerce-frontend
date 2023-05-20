29dba382d2b83123db351343ec207a467629de5b
vm2124 <vm21242124@gmail.com>
2023-05-20 13:13:37 +0530

    updating routes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import LoginPage from "./Pages/Login/LoginPage";
 import RegisterPage from "./Pages/Register/RegisterPage";
 import AdminDashboard from "./Pages/Admin/AdminDashboard";
 import axios from "axios";
-import { useSelector } from "react-redux";
+
 import UserCart from "./Pages/UserCart/UserCart";
+import ProductPage from "./Pages/ProductPage/ProductPage";
+import SearchPage from "./Pages/SearchPage/SearchPage";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const user = useSelector((state) => state.user.user);
-  console.log(user);
   return (
     <>
       <BrowserRouter>
@@ -20,9 +20,10 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/register" element={<RegisterPage />} />
-
           <Route exact path="/admin/dashboard" element={<AdminDashboard />} />
           <Route exact path="/cart" element={<UserCart/>} />
+          <Route exact path="/product/:id" element={<ProductPage/>}/>
+          <Route exact path="/search/:search" element={<SearchPage/>}/>
 
         </Routes>
       </BrowserRouter>
