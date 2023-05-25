fa501df785d06b5dc79c6ed45947f7d17b975ed8
vm2124 <vm21242124@gmail.com>
2023-05-25 12:22:33 +0530

    fixing styles

--- src/Components/TopProduct/TopProducts.jsx
+++ src/Components/TopProduct/TopProducts.jsx
@@ -7,12 +7,12 @@ import { appendStringAfterLength } from '../../UtilityFunctions'
 
 
 const TopProducts = () => {
-    const [topProduct,setTopProducts]=useState([])
-    useEffect(()=>{
-        axios.get(`/product/645b1c3bb5f32f212614414c`).then((res)=>setTopProducts(res.data.products)).catch((e)=>console.log(e))
+    const [topProduct, setTopProducts] = useState([])
+    useEffect(() => {
+        axios.get(`/product/645b1c3bb5f32f212614414c`).then((res) => setTopProducts(res.data.products)).catch((e) => console.log(e))
 
-},[])
-    
+    }, [])
+
     return (
         <div className="TopProducts">
             <div className="heading">
@@ -21,35 +21,35 @@ const TopProducts = () => {
 
             </div>
             <div className="products">
-            {topProduct.map((item,i)=>(
-                    <Product key={i} id={item._id} name={item.name} price={item.price} img={item.photos[0].secure_url}/>
+                {topProduct.map((item, i) => (
+                    <Product key={i} id={item._id} name={item.name} price={item.price} img={item.photos[0].secure_url} />
                 ))}
             </div>
         </div>
     )
 }
-export const Product=({id,name,img,price})=>{
-    const nav=useNavigate()
-    const dispatch=useDispatch();
-    const handleAddToCart=(e)=>{
+export const Product = ({ id, name, img, price }) => {
+    const nav = useNavigate()
+    const dispatch = useDispatch();
+    const handleAddToCart = (e) => {
         e.preventDefault();
-        const product={id,name,img,price}
-       dispatch({
-        type:"addToCart",
-        payload:product
-       })
+        const product = { id, name, img, price }
+        dispatch({
+            type: "addToCart",
+            payload: product
+        })
 
     }
-    const pname=appendStringAfterLength(name,50,".....")
-    return(
+    const pname = appendStringAfterLength(name, 50, ".....")
+    return (
         <div className="product">
             <div>
-                <img onClick={()=>nav(`/product/${id}`)} src={img} alt="" className="pp" />
+                <img onClick={() => nav(`/product/${id}`)} src={img} alt="" className="pp" />
             </div>
-            <h4 onClick={()=>nav(`/product/${id}`)}>{pname}</h4>
+            <h4 onClick={() => nav(`/product/${id}`)}>{pname}</h4>
             <p className='ppe'>₹ <h5>{price}</h5></p>
-        
-        <button onClick={handleAddToCart} className="lgbutton">add to cart</button>
+
+            <button onClick={handleAddToCart} className="lgbutton">add to cart</button>
         </div>
     )
 }
