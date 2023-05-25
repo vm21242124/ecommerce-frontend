78f612403b09cb310f486bab0167d1f5b3496fbf
vm2124 <vm21242124@gmail.com>
2023-05-25 12:23:15 +0530

    searchpage fixing

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import './SearchPage.css'
 import Navbar from '../../Components/Header/Navbar'
 const SearchPage = () => {
     const { search } = useParams()
     const [product, setProducts] = useState([])
+    const nav=useNavigate()
     useEffect(() => {
         axios.get(`/product/search?q=${search}`).then((res) => setProducts(res.data.products)).catch((e) => console.log(e.message))
     })
@@ -18,9 +19,9 @@ const SearchPage = () => {
                     <h4>Product Match {product.length}</h4>
                     {product.length !== 0 ? product.map((item, i) => (
 
-                        <div key={i} className="searchproduct">
-                            <img className='ww' src={item.photos[0].secure_url} alt="s" />
-                            <h3>{item.name}</h3>
+                        <div key={i} className="searchproduct" style={{cursor:"pointer"}}>
+                            <img onClick={() => nav(`/product/${item?._id}`)} className='ww' src={item.photos[0].secure_url} alt="s" />
+                            <h3 onClick={() => nav(`/product/${item?._id}`)}>{item.name}</h3>
                         </div>
                     )) : <div className='nf'><h1>search product not available</h1>
                     </div>}
