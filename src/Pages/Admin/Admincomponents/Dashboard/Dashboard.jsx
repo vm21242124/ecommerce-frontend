1a6e759277cfbf88fb8ce1f3300140c760373e8c
vm2124 <vm21242124@gmail.com>
2023-06-07 13:38:10 +0530

    fun - getting all orders in admin panel

--- src/Pages/Admin/Admincomponents/Dashboard/Dashboard.jsx
+++ src/Pages/Admin/Admincomponents/Dashboard/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React, { } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
+import axios from 'axios';
+import {AiOutlineEye,AiOutlineDelete} from 'react-icons/ai'
+import {BiEditAlt} from 'react-icons/bi'
+
+
 const Dashboard = () => {
-  // const [data,setData]=useState([]);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("/order/all").then((res) => setData(res.data.orders)).catch(e => console.log(e.message))
+  })
+  console.log(data);
 
   return (
     <div className="dashboard">
@@ -14,10 +23,26 @@ const Dashboard = () => {
       </div>
       <div className="DashboardOrders">
         <div className="DashboardOrdersTop">
-          <span className="orderHeading">sn</span><span className="orderHeading">order id</span><span className="orderHeading">custumer name</span><span className="orderHeading">status</span><span className="orderHeading">date</span><span className="orderHeading">price</span>action<span className="orderHeading"></span>
+          <span className="orderHeading">sn</span><span className="orderHeading">order id</span><span className="orderHeading">status</span><span className="orderHeading">date</span><span className="orderHeading">price</span><span className="orderHeading">action</span>
         </div>
         <div className="DashboardOrdersScroll">
-          <p>there are no orders yet</p>
+          {data.map((item, i) => (
+            <div className="DashboardOrdersTop" key={i}>
+              <span className="orderHeading">{i}</span><span className="orderHeading">{item._id}</span><span className="orderHeading">{item.status}</span><span className="orderHeading">{item.createdAt}</span><span className="orderHeading">{item.amount}</span>
+              
+              <div className='topindxing5'>
+                    <span>
+                        <AiOutlineEye />
+                    </span>
+                    <span>
+                        <BiEditAlt />
+                    </span>
+                    <span >
+                        <AiOutlineDelete />
+                    </span>
+                </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
